ecc35ca179624a1e5740b8212e5bff29e55dcf7b
Nihaal Sangha <nihaal.s@live.co.uk>
2017-03-19 18:20:08 +0000

    Added reaction to s.ack

--- cogs/ack.py
+++ cogs/ack.py
@@ -6,12 +6,21 @@ class Ack():
 
     @commands.command()
     async def ack(self, ctx, server = None):
+        servers = None
+        await ctx.message.add_reaction("\U00002705")
         if not server:
             for server in self.bot.guilds:
                 await server.ack()
+                servers = False
         else:
             await ctx.guild.ack()
-        await ctx.message.edit(content="All Messages are now read")
+            servers = True
+
+        if servers is True:
+            await ctx.message.edit(content="All Messages are now read")
+        else:
+            await ctx.message.edit(content="This server is now read")
+        await ctx.message.clear_reactions()
 
 def setup(bot):
     bot.add_cog(Ack(bot))
