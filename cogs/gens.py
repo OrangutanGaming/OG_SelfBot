f1abcd2efe82164a3132291fc0418532b7bb94d1
Nihaal Sangha <nihaal.s@live.co.uk>
2017-03-27 17:23:46 +0100

    Added gen for servers

--- cogs/gens.py
+++ cogs/gens.py
@@ -4,20 +4,36 @@ class Gens():
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command()
-    async def count(self, ctx, option : str, value):
+    @commands.command(aliases = ["countuser", "countusers"])
+    async def countu(self, ctx, option : str, value):
         if not option:
             await ctx.message.edit(content="Options missing")
             return
 
         option = option.lower()
 
-        if option == "disc" or "discrim":
+        if option == "disc" or option == "discrim":
             option = "discriminator"
 
-        output = ", ".join(x.name + "#" + x.discriminator for x in self.bot.users if getattr(x, option) == value)
+        output = ", ".join(str(x) for x in self.bot.users if getattr(x, option) == value)
 
-        output = f"All members with the {option.title()} of {value} on the same servers as `OGaming#7135`: " + "```" + output + "```"
+        if not output: output = f"There are no members with the {option.title()} of {value}"
+        else: output = f"All members with the {option.title()} of {value} on the same servers as `OGaming#7135`: " + "```" + output + "```"
+
+        await ctx.message.edit(content=output)
+
+    @commands.command(aliases = ["countserver", "countservers"])
+    async def counts(self, ctx, option: str, *, value):
+        if not option:
+            await ctx.message.edit(content="Options missing")
+            return
+
+        option = option.lower()
+
+        output = ", ".join(x.name + "({})".format(x.id) for x in self.bot.guilds if getattr(x, option) == value)
+
+        if not output: output = f"There are no servers with the {option.title()} of {value}"
+        else: output = f"All servers with the {option.title()} of {value} that `OGaming#7135` is part of: " + "```" + output + "```"
 
         await ctx.message.edit(content=output)
 
