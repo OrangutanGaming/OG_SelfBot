3b08d773089e7e20e5e86e7b03733875782d638b
Nihaal Sangha <nihaal.s@live.co.uk>
2017-03-27 17:24:26 +0100

    Made s.hepbot easier to use Added better BlackList

--- cogs/hepbot.py
+++ cogs/hepbot.py
@@ -7,7 +7,8 @@ class HepBot():
         self.bot = bot
 
     async def on_message(self, message):
-        if "Here, take some credits. Enjoy!" in message.content:
+        if "Here, take some credits. Enjoy!" in message.content \
+                or "Too soon. For your next payday you have to wait" in message.content:
             await message.ack()
 
         # def check(message):
@@ -24,11 +25,22 @@ class HepBot():
         tmp = await ctx.send("Going...")
         counter = 0
         channels = []
+
+        banned_ids = [290953743233581057, # WarChat
+                      166488311458824193, # RV
+                      110373943822540800, # Discord Bots
+                      213819570656378880, # WFRSB
+                      81384788765712384, # Discord API
+                      109379086845009920, # V (old)
+                      281968634086031363 # V (New)
+
+                      ]
+        banned_channel_names=["music", "hidden", "mod"]
         for server in self.bot.guilds:
             if discord.utils.find(lambda m: m.name == "HepBot", server.members):
                 for channel in server.text_channels:
                     if "bot" in channel.name and "spam" in channel.name\
-                            and not "music" in channel.name and not "hidden" in channel.name and not "mod" in channel.name:
+                            and not channel.name in banned_channel_names and not server.id in banned_ids:
                         channels.append(channel)
                         break
                     else:
@@ -41,7 +53,10 @@ class HepBot():
                 continue
 
         for botspam in channels:
-            await botspam.send("!payday")
+            try:
+                await botspam.send("!payday")
+            except discord.Forbidden as error:
+                print(f"{error}: {botspam.name}, {botspam.id}, {botspam.guild.name}")
             counter += 1
 
         await tmp.edit(content=f"Done. Sent to {counter} servers.")
