9deed70d4507b46fb88ef3d6a4e33efddd678591
Nihaal Sangha <nihaal.s@live.co.uk>
2017-03-27 17:23:31 +0100

    Added Charinfo

--- cogs/fun.py
+++ cogs/fun.py
@@ -2,6 +2,7 @@ import discord
 from discord.ext import commands
 import time
 import asyncio
+import unicodedata
 
 class Fun():
     def __init__(self, bot):
@@ -28,6 +29,22 @@ class Fun():
     async def stat_test(self, ctx):
         await self.bot.change_presence(game=None)
         await ctx.message.edit(content="Playing set to None")
+
+    @commands.command()
+    async def charinfo(self, ctx, *, characters: str):
+
+        if len(characters) > 15:
+            await ctx.send(f"Too many characters ({len(characters)}/15)")
+            return
+
+        fmt = "`\\U{0:>08}`: {1} - {2} \N{EM DASH} <http://www.fileformat.info/info/unicode/char/{0}>"
+
+        def to_string(c):
+            digit = format(ord(c), "x")
+            name = unicodedata.name(c, "Name not found.")
+            return fmt.format(digit, name, c)
+
+        await ctx.send("\n".join(map(to_string, characters)))
 
 def setup(bot):
     bot.add_cog(Fun(bot))
