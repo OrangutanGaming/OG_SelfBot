4ef117adfc462174a9d011c9c11648b40c27effa
Nihaal Sangha <nihaal.s@live.co.uk>
2017-03-27 19:01:28 +0100

    Added errors for role not found

--- cogs/serverinfo.py
+++ cogs/serverinfo.py
@@ -6,14 +6,29 @@ class ServerInfo():
         self.bot = bot
 
     @commands.command(aliases=["rolecount"])
-    async def rolec(self, ctx, role: discord.Role = None, list = False, mention = False):
+    async def rolec(self, ctx, oRole: str = None, list = False, mention = False):
+
+        def status(user):
+            if user.status is discord.Status.offline:
+                return ":notebook:"
+            elif user.status is discord.Status.idle:
+                return ":orange_book:"
+            elif user.status is discord.Status.online:
+                return ":green_book:"
+            elif user.status is discord.Status.dnd:
+                return ":closed_book:"
+            else:
+                return ""
+
         online = 0
-        if not role:
+        if not oRole:
             await ctx.message.edit(content="You must specify a role")
             return
 
+        role = discord.utils.get(ctx.guild.roles, name=oRole)
+
         if not role:
-            await ctx.message.edit(content=f"Can't find the role {role}")
+            await ctx.message.edit(content=f"Can't find the role {oRole}")
             return
 
         counter = len(role.members)
@@ -25,10 +40,29 @@ class ServerInfo():
 
         if list:
             names=[]
-            if mention: names = ", ".join(user.mention for user in role.members)
-            else: names = ", ".join(f"`{str(user)}`" for user in role.members)
+
+            for user in role.members:
+                uStatus = status(user)
+                if mention: names.append(f"{user.mention} {uStatus}")
+                else: names.append(f"{str(user)} {uStatus}")
+            names = ", ".join(names)
+            if len(names) > 1981: # msg doesn't breach 2k char limit
+                return
 
             await ctx.send(f"Their names are: {names}")
+
+    # @rolec.error
+    # async def handler(self, error, ctx):
+    #     await ctx.message.edit(content="Can't find the role {role}")
+
+    # @commands.command()
+    # async def command(self, arg: discord.Role):
+    #     return
+    #
+    # @command.error
+    # async def handler(self, error, ctx):
+    #     if error is discord.ext.commands.errors.BadArgument:
+    #         print(ctx.kwargs.get("arg"))
 
 def setup(bot):
     bot.add_cog(ServerInfo(bot))
