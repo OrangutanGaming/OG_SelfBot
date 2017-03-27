7316e82ff7e456c03ec0d00893036ac50f697b3a
Nihaal Sangha <nihaal.s@live.co.uk>
2017-03-27 17:22:34 +0100

    Made s.ack neater and removed server

--- cogs/ack.py
+++ cogs/ack.py
@@ -1,26 +1,20 @@
 from discord.ext import commands
+import asyncio
 
 class Ack():
     def __init__(self, bot):
         self.bot = bot
 
     @commands.command()
-    async def ack(self, ctx, server = None):
-        servers = None
+    async def ack(self, ctx):
         await ctx.message.add_reaction("\U00002705")
-        if not server:
-            for server in self.bot.guilds:
-                await server.ack()
-                servers = False
-        else:
-            await ctx.guild.ack()
-            servers = True
+        for server in self.bot.guilds:
+            await server.ack()
 
-        if servers is True:
-            await ctx.message.edit(content="All Messages are now read")
-        else:
-            await ctx.message.edit(content="This server is now read")
-        await ctx.message.clear_reactions()
+        await ctx.message.edit(content="All Messages are now read")
+        await ctx.message.remove_reaction("\U00002705", ctx.guild.me)
+        await asyncio.sleep(3)
+        await ctx.message.delete()
 
 def setup(bot):
     bot.add_cog(Ack(bot))
