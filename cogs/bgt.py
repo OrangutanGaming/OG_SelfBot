8168da669ea509fd1f2e91248e1f51b2ab910ce9
Nihaal Sangha <nihaal.s@live.co.uk>
2017-03-27 17:22:45 +0100

    Disabled BGT

--- cogs/bgt.py
+++ cogs/bgt.py
@@ -5,15 +5,18 @@ import discord
 class BGT():
     def __init__(self, bot):
         self.bot = bot
-        self.bg_task = self.bot.loop.create_task(self.my_background_task())
+        # self.bg_task = self.bot.loop.create_task(self.my_background_task())
 
-    async def my_background_task(self):
-        await self.bot.wait_until_ready()
-        while True:
-            server = discord.utils.get(self.bot.guilds, id=281483874234793984)
-            channel = discord.utils.get(server.text_channels, id=281483874234793984)
-            await channel.send("s.hepbot")
-            await asyncio.sleep((5*60))
+    # async def my_background_task(self):
+    #     await self.bot.wait_until_ready()
+    #     await asyncio.sleep(5)
+    #     while True:
+    #         await self.bot.get_channel(293128108570050560).send("s.hepbot")
+    #         await asyncio.sleep(float(5*60))
+
+    @commands.command()
+    async def custom(self, ctx, id : int, msg : str):
+        await self.bot.get_channel(id).send(msg)
 
 def setup(bot):
     bot.add_cog(BGT(bot))
