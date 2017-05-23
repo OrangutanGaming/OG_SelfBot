53e22ea2aeaecddc74a1e7f6140e51ba36d9e2f0
Nihaal Sangha <nihaal.s@live.co.uk>
2017-05-23 20:35:13 +0100

    Added to auto glyph redeemer

--- cogs/glyph.py
+++ cogs/glyph.py
@@ -1,19 +1,40 @@
 from discord.ext import commands
 import re
-from Naked.toolshed.shell import execute_js
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
 class Glyph():
     def __init__(self, bot):
         self.bot = bot
 
+    def enterCode(self, code):
+        try:
+            driver = webdriver.Firefox()
+            driver.get(f"https://www.warframe.com/promocode?code={code}")
+            elem = driver.find_element_by_name("code")
+            elem.send_keys(Keys.RETURN)
+            assert "already been redeemed" not in driver.page_source
+            assert "please try again" not in driver.page_source
+            # Check if page is now success page (https://www.warframe.com/redeem_success?item={})
+            driver.close()
+            return True
+        except AssertionError:
+            return False
+
     async def on_message(self, message):
         content = message.content
-        x = (r"?")
-        try: x[0]
+        try: x = re.findall(r"([A-Z0-9]{4})-([A-Z0-9]{4})-([A-Z0-9]{4})-([A-Z0-9]{4})", content)
         except IndexError:
             return
+        fullCode=[]
         for code in x:
-            execute_js("", "")
+            for fragment in code:
+                fullCode.append(fragment)
+            final = final + "-".join(fullCode)
+        for code in x:
+            if self.enterCode(code):
+                await self.bot.get_channel(315579438563852289)\
+                    .send(f"{self.bot.user.mention} Success! Used code `{code}`!")
 
 def setup(bot):
     bot.add_cog(Glyph(bot))
