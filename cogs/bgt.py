7ad3cb5a19f0232d5857f930f46f91e2277d0d5b
Nihaal Sangha <nihaal.s@live.co.uk>
2017-06-05 08:03:01 +0100

    Testing bgts

--- cogs/bgt.py
+++ cogs/bgt.py
@@ -1,18 +1,39 @@
 from discord.ext import commands
 import asyncio
 import discord
+import datetime
 
 class BGT():
     def __init__(self, bot):
         self.bot = bot
-        # self.bg_task = self.bot.loop.create_task(self.my_background_task())
+        # self.bg_task = self.bot.loop.create_task(self.AllianceAnnounceStart())
+        # self.bg_task = self.bot.loop.create_task(self.AllianceAnnounceEnd())
+        self.bg_task = self.bot.loop.create_task(self.TimeTest())
 
-    # async def my_background_task(self):
-    #     await self.bot.wait_until_ready()
-    #     await asyncio.sleep(5)
-    #     while True:
-    #         await self.bot.get_channel(293128108570050560).send("s.hepbot")
-    #         await asyncio.sleep(float(5*60))
+    async def AllianceAnnounceStart(self):
+        await self.bot.wait_until_ready()
+        while True:
+            now = datetime.datetime.utcnow()
+            if now.date() == datetime.date(2017, 6, 12) and now.time() >= datetime.time(7):
+                await self.bot.get_channel(282207136354926594).send("@everyone And so it begins!")
+                break
+
+    async def AllianceAnnounceEnd(self):
+        await self.bot.wait_until_ready()
+        while True:
+            now = datetime.datetime.utcnow()
+            if now.date() == datetime.date(2017, 6, 12) and now.time() >= datetime.time(7):
+                await self.bot.get_channel(282207136354926594).send("@everyone And so it begins!")
+                break
+
+    async def TimeTest(self):
+        await self.bot.wait_until_ready()
+        while True:
+            now = datetime.datetime.utcnow()
+            if now.date() == datetime.date(2017, 6, 12) and now.time() >= datetime.time(6, 4):
+                await self.bot.get_channel(281483874234793984).send("Hi")
+                break
+
 
     @commands.command()
     async def custom(self, ctx, id : int, msg : str):
